c33fdd9669b93d82467ed67153e4349dcf92b6d7
Heresy <kheresy@gmail.com>
2016-10-05 14:42:57 +0800

    system() version

--- boost.process.cpp
+++ boost.process.cpp
@@ -4,8 +4,5 @@
 
 int main(int argc, char** argv)
 {
-	boost::process::child c("ping 127.0.0.1");
-	c.wait();
-
-	std::cout << c.exit_code() << std::endl;
+	std::cout << boost::process::system("ping 127.0.0.1") << std::endl;
 }
