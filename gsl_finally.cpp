94234a61f46a68671f0afdded783d67f7ce0b7d6
Heresy <kheresy@gmail.com>
2019-03-20 16:34:21 +0800

    Create gsl_finally.cpp

---
+++ gsl_finally.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+#include <gsl/gsl_util>
+
+class CData
+{
+public:
+	CData()
+	{
+		std::cout << " > CData created" << std::endl;
+	}
+
+	~CData()
+	{
+		std::cout << " > CData released" << std::endl;
+	}
+};
+
+
+int leak( int iInput)
+{
+	CData* pData = new CData();
+
+	if (iInput > 0)
+		return 1;
+
+	delete pData;
+	return -1;
+}
+
+int noleak(int iInput)
+{
+	CData* pData = new CData();
+	auto _ = gsl::finally([pData] {
+		delete pData;
+	});
+
+	if (iInput > 0)
+		return 1;
+
+	delete pData;
+	return -1;
+}
+
+int main()
+{
+	std::cout << "May leak" << std::endl;
+	leak(1);
+
+	std::cout << "No leak with gsl::finally" << std::endl;
+	noleak(1);
+}
